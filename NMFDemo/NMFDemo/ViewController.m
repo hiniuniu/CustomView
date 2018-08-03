462ab7d52dda8ad0bdd0afb8c128a7fd541e00e8
hiniuniu <1484740543@qq.com>
2018-08-03 10:24:09 +0800

    Update ViewController.m

--- NMFDemo/NMFDemo/ViewController.m
+++ NMFDemo/NMFDemo/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "ChildView.h"
+#import "CustomView.h"
 
 @interface ViewController ()
 
@@ -18,15 +19,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setUI];
+}
+
+- (void)setUI {
+    CustomView *customView = [[CustomView alloc] initWithFrame:CGRectMake(0, 200, self.view.frame.size.width, 100)];
+    customView.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:customView];
+    
+    __weak typeof(self) weakSelf = self;
+    customView.clickCustomButtomBlock = ^{
+        NSLog(@"hahahha");
+        __strong typeof(weakSelf) strongSelf = weakSelf;
+        [strongSelf showChildView];
+    };
 }
 
 
 - (IBAction)buttonAction:(id)sender {
-    
+    [self showChildView];
+}
+
+- (void)showChildView {
     ChildView *selectView = [[ChildView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
     //显示
     [selectView showInView:self.navigationController.view];
-    
 }
 
 
